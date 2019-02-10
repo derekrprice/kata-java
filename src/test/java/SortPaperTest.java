8db6f83b6219f357db439bb8d380f529fd8a93c9
Derek Price <derek.price@dxc.com>
2019-02-10 00:38:53 -0500

    Test row count.

--- src/test/java/SortPaperTest.java
+++ src/test/java/SortPaperTest.java
@@ -1,18 +1,30 @@
 import org.apache.commons.csv.CSVRecord;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 public class SortPaperTest {
+    private List<CSVRecord> csvRecords;
+
+    @BeforeEach
+    public void setUp() {
+        csvRecords = SortPaper.getCSV("Catalog.csv");
+    }
+
     @Test
     void testHeaders() {
-        List<CSVRecord> csvRecords = SortPaper.getCSV("Catalog.csv");
         Assertions.assertEquals("part #", csvRecords.get(0).get(0));
         Assertions.assertEquals("Description", csvRecords.get(0).get(1));
         Assertions.assertEquals("Category", csvRecords.get(0).get(2));
         Assertions.assertEquals("UOM", csvRecords.get(0).get(3));
         Assertions.assertEquals("Price", csvRecords.get(0).get(4));
         Assertions.assertEquals("Discount", csvRecords.get(0).get(5));
+    }
+
+    @Test
+    void testRowCount() {
+        Assertions.assertEquals(13, csvRecords.size());
     }
 }
