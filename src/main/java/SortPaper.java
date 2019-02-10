d33a13e03b779df39715a3958dc2873f001c9939
Derek Price <derek.price@dxc.com>
2019-02-10 00:21:48 -0500

    Open the CSV and prove we can read headers.

--- src/main/java/SortPaper.java
+++ src/main/java/SortPaper.java
@@ -1,12 +1,38 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SortPaper {
     public static void main (String[] args) {
         if (args.length < 1) {
             System.err.print("File name is a required argument.");
             System.exit(2);
         }
+
+        for (String fileName : args) {
+            List<CSVRecord> products = SortPaper.getCSV(fileName);
+        }
     }
 
-    public static String getFile (String fileName) {
-        return "a string";
+    public static List<CSVRecord> getCSV (String fileName) {
+        List<CSVRecord> out = new ArrayList<CSVRecord>();
+        try (
+            Reader reader = Files.newBufferedReader(Paths.get(fileName));
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+        ) {
+            for (CSVRecord csvRecord : csvParser) {
+                out.add(csvRecord);
+            }
+        } catch (IOException e ) {
+
+        }
+        return out;
     }
 }
--- src/test/java/SortPaperTest.java
+++ src/test/java/SortPaperTest.java
@@ -1,9 +1,18 @@
+import org.apache.commons.csv.CSVRecord;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class SortPaperTest {
     @Test
-    void testHello() {
-        Assertions.assertEquals("a string", SortPaper.getFile("test"));
+    void testHeaders() {
+        List<CSVRecord> csvRecords = SortPaper.getCSV("Catalog.csv");
+        Assertions.assertEquals("part #", csvRecords.get(0).get(0));
+        Assertions.assertEquals("Description", csvRecords.get(0).get(1));
+        Assertions.assertEquals("Category", csvRecords.get(0).get(2));
+        Assertions.assertEquals("UOM", csvRecords.get(0).get(3));
+        Assertions.assertEquals("Price", csvRecords.get(0).get(4));
+        Assertions.assertEquals("Discount", csvRecords.get(0).get(5));
     }
 }
